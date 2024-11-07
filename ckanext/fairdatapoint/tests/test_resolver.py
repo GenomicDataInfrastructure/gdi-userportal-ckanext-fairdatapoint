2a7d7404d83f250b444a58710538801969ce29c7
Mark Janse <mark.janse@health-ri.nl>
2024-11-07 20:24:05 +0100

    First unit test of graph loader

--- ckanext/fairdatapoint/tests/test_resolver.py
+++ ckanext/fairdatapoint/tests/test_resolver.py
@@ -1,7 +1,36 @@
 # SPDX-FileCopyrightText: 2024 Stichting Health-RI
 #
 # SPDX-License-Identifier: AGPL-3.0-only
+from pathlib import Path
+
 import pytest
 import requests_mock
+from rdflib import Graph
+from rdflib.compare import to_isomorphic
 
-# TODO
+from ckanext.fairdatapoint.resolver import resolvable_label_resolver
+
+TEST_DATA_DIRECTORY = Path(Path(__file__).parent.resolve(), "test_data")
+
+
+class TestGenericResolverClass:
+
+    def test_graph_loader(self):
+        resolver = resolvable_label_resolver()
+        with open(Path(TEST_DATA_DIRECTORY, "wikidata_data_catalog_entry.ttl")) as file:
+            with requests_mock.Mocker() as mock:
+                mock.get(
+                    "http://www.wikidata.org/entity/Q29937289",
+                    body=file,
+                    headers={"content-type": "text/turtle"},
+                )
+
+                reference_graph = Graph().parse(
+                    Path(TEST_DATA_DIRECTORY, "wikidata_data_catalog_entry.ttl")
+                )
+
+                new_graph = resolver.load_graph(
+                    "http://www.wikidata.org/entity/Q29937289"
+                )
+
+                assert to_isomorphic(reference_graph) == to_isomorphic(new_graph)
