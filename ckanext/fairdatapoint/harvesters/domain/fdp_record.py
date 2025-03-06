0c357684e340fe4c41f19bb70b89834b4a6fb602
Hans-Chrstian <hanschristian.vanderwerf@health-ri.nl>
2025-03-06 22:38:26 +0100

    fix license fdprecord

--- ckanext/fairdatapoint/harvesters/domain/fdp_record.py
+++ ckanext/fairdatapoint/harvesters/domain/fdp_record.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2023 Civity
 # SPDX-FileContributor: 2024 Stichting Health-RI
 #
-# SPDX-License-Identifier: AGPL-3.0-onl
+# SPDX-License-Identifier: AGPL-3.0-only
 
 from rdflib import DCAT, RDF, URIRef
 
--- ckanext/fairdatapoint/harvesters/domain/graph_to_fdp_record_mapper.py
+++ ckanext/fairdatapoint/harvesters/domain/graph_to_fdp_record_mapper.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2023 Civity
 # SPDX-FileContributor: 2024 Stichting Health-RI
 #
-# SPDX-License-Identifier: AGPL-3.0-onl
+# SPDX-License-Identifier: AGPL-3.0-only
 
 from ckanext.fairdatapoint.harvesters.domain.fdp_record import FdpRecord
 from rdflib import Namespace
